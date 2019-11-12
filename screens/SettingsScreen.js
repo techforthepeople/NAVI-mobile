98bee9dd7a2cc9ec1bd349788a8df909dafc6103
Vishnu Ravi <vishnu.ravi@gmail.com>
2019-11-12 00:56:54 -0500

    Create webview for sensor admin page

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,15 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native'
+import { WebView } from 'react-native-webview';
 
 export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
   return (
-    <View>
-      <Text>UNDER DEVELOPMENT</Text>
-    </View>
+    <WebView source={{ uri: 'http://raspberrypi.local:5000/' }} />
   );
 }
 
